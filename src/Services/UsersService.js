f3fa922f5ca54c5bbe4329ca2a2c1fb6c10e5a97
XJXlbtOT <pavlo.poliuha@gmail.com>
2021-09-05 19:43:58 +0300

    SortUser dropdown initial layout

--- src/Services/UsersService.js
+++ src/Services/UsersService.js
@@ -1,13 +1,19 @@
 import { api } from "./../Config/Axios";
 
+
 const GetAllUsers = () => {
   return api().get("Users/GetAllUsers");
 };
 const SearchUser = (name) => {
   return api().get(`/Users/SearchUser?name=${name}`)
 };
+const GetSortedUsers = (data) => {
+  return api().get(`/Users/GetSortedUsers`)
+}
+export default {GetSortedUsers};
 
 export default {
   GetAllUsers,
   SearchUser,
 };
+
