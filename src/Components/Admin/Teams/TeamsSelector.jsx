d8ae65422e37a19393b646a8bce92a3e8bd51f2b
Your Name <ruslanmonar@gmail.com>
2021-09-23 00:45:19 +0300

    dropzone

--- src/Components/Admin/Teams/TeamsSelector.jsx
+++ src/Components/Admin/Teams/TeamsSelector.jsx
@@ -1,7 +1,10 @@
 import Dropdown from "react-dropdown";
 import "../../../css/Admin/Teams.css";
+import Dropzone from "./DropZone";
+import { useState } from "react";
 
 const TeamsSlector = () => {
+  const [file, setFile] = useState([]);
   const options = [
     {
       value: "USA",
@@ -13,24 +16,60 @@ const TeamsSlector = () => {
     },
   ];
 
+  const handleDropZoneChange = (files) => {
+    setFile(
+      files.map((file) =>
+        Object.assign(file, {
+          preview: URL.createObjectURL(file),
+        })
+      )
+    );
+  };
+
   return (
     <div className="dropdowns-container">
       <div className="dropdown-option">
         <span>SELECT LOCATION</span>
-        <Dropdown className="teams-selector" options={options} placeholder="Select an option" />
+        <Dropdown
+          className="teams-selector"
+          options={options}
+          placeholder="Select an option"
+        />
       </div>
       <div className="dropdown-option">
         <span>SELECT CATEGORY</span>
-        <Dropdown className="teams-selector"  options={options} placeholder="Select an option" />
+        <Dropdown
+          className="teams-selector"
+          options={options}
+          placeholder="Select an option"
+        />
       </div>
       <div className="dropdown-option">
         <span>SELECT SUBCATEGORY</span>
-        <Dropdown className="teams-selector" options={options} placeholder="Select an option" />
+        <Dropdown
+          className="teams-selector"
+          options={options}
+          placeholder="Select an option"
+        />
       </div>
       <div className="dropdown-option">
         <span>TEAM</span>
-        <Dropdown className="teams-selector" options={options} placeholder="Select an option" />
+        <Dropdown
+          className="teams-selector"
+          options={options}
+          placeholder="Select an option"
+        />
       </div>
+      
+      <Dropzone onChange={handleDropZoneChange} />
+
+      <div
+        onClick={() => console.log(file)}
+        className="teams-dropdown-button teams-dropdown-button-red"
+      >
+        Add to list
+      </div>
+      <div className="teams-dropdown-button">Cancel</div>
     </div>
   );
 };
