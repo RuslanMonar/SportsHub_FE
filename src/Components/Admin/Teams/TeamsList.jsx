6de9730dfb69d0318965996ae32ce0856745c798
XJXlbtOT <pavlo.poliuha@gmail.com>
2021-09-24 20:37:14 +0300

    Modified pagination option and dropdown

--- src/Components/Admin/Teams/TeamsList.jsx
+++ src/Components/Admin/Teams/TeamsList.jsx
@@ -5,10 +5,11 @@ import { useState } from "react";
 import ReactPaginate from 'react-paginate';
 
 export const TeamsList = () => {
-    let teams = [], max = 50;
+    const [loader, setLoader] = useState(true);
+    let teams = [], max = 200;
     for(var i=1; teams.push(i++) < max;);
     const [pageNumber, setPageNumber] = useState(0)
-    const options = [10, 15, 25, 50];
+    const options = [10, 25, 50];
     const [teamsPerPage, setTeamsPerPage] = useState(options[0]);
 
     const changeTeamsPerPage = (e) =>{
@@ -19,73 +20,119 @@ export const TeamsList = () => {
     }
     const pageVisited = pageNumber * teamsPerPage;
 
-    const displayTeams = teams
+    const allTeams = teams
         .slice(pageVisited, pageVisited + teamsPerPage)
         .map((team) => {
             return(
-                <div>
-                    <h1> {team} </h1>
+                <div className={"teams-list-item align-center"}>
+                    <div className={"flex team align-center"}>
+                        <h1> {team} </h1>
+                    </div>
                 </div>
             );
         })
 
     const [pageCount, setPageCount] = useState(Math.ceil(teams.length / teamsPerPage))
     const changePage = ({selected}) => {
-
         setPageNumber(selected)
     }
     const changePageResultStatus = ({currentPage, pagesCount, teamsCount}) =>{
         return(
-            <div> {currentPage} - {pagesCount} of {teamsCount} results </div>
+            <div className={"current-page-line"}> {currentPage} - {pagesCount} of {teamsCount} results </div>
         );
     } 
-    return (
-        <><div className="flex team-list-container">
-            <div className="flex teams-list-item align-center list-header">
-                <div className="header flex list-header-font">
-                    <span>TEAMS</span>
-                </div>
-                <div className="header flex list-header-font">
-                    <span>LOCATION</span>
-                </div>
-                <div className="header flex list-header-font">
-                    <span>ADDED AT</span>
-                </div>
-                <div className="header flex list-header-font">
-                    <span>CATEGORY</span>
-                </div>
-                <div className="header flex list-header-font">
-                    <span>SUBCATEGORY</span>
-                </div>
-                <div className="header flex list-header-font">
-                    <span></span>
+    const displayTeams = () => {
+        if(teams.length === 0){
+            return(
+                <div></div>
+            );
+        }
+        else if(teams.length < options[0]){
+            return(
+                <> <div className="flex team-list-container">
+                        <div className="flex teams-list-item align-center teams-list-header">
+                            <div className="teams-header flex teams-list-header-font">
+                                <span>TEAMS</span>
+                            </div>
+                            <div className="teams-header flex teams-list-header-font">
+                                <span>LOCATION</span>
+                            </div>
+                            <div className="teams-header flex teams-list-header-font">
+                                <span>ADDED AT</span>
+                            </div>
+                            <div className="teams-header flex teams-list-header-font">
+                                <span>CATEGORY</span>
+                            </div>
+                            <div className="teams-header flex teams-list-header-font">
+                                <span>SUBCATEGORY</span>
+                            </div>
+                            <div className="teams-header flex teams-list-header-font">
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                    {allTeams}
+                    </div> </>
+            );
+        }
+        return (
+            <><div className="flex team-list-container">
+                <div className="flex teams-list-item align-center teams-list-header">
+                    <div className="teams-header flex teams-list-header-font">
+                        <span>TEAMS</span>
+                    </div>
+                    <div className="teams-header flex teams-list-header-font">
+                        <span>LOCATION</span>
+                    </div>
+                    <div className="teams-header flex teams-list-header-font">
+                        <span>ADDED AT</span>
+                    </div>
+                    <div className="teams-header flex teams-list-header-font">
+                        <span>CATEGORY</span>
+                    </div>
+                    <div className="teams-header flex teams-list-header-font">
+                        <span>SUBCATEGORY</span>
+                    </div>
+                    <div className="teams-header flex teams-list-header-font">
+                        <span></span>
+                    </div>
                 </div>
             </div>
-        </div>
-        {displayTeams}
-        <div className="flex team-list-container">
-            <div className="flex teams-list-item align-center list-header">
-                <ReactPaginate
-                    previousLabel={"<"}
-                    nextLabel={">"}
-                    pageCount={pageCount}
-                    onPageChange={changePage}
-                    containerClassName={"paginationBttns"}
-                    previousLinkClassName={"previousBttn"}
-                    nextLinkClassName={"nextBttn"}
-                    activeClassName={"paginationActive"}
-                ></ReactPaginate>
-                {changePageResultStatus({currentPage: pageNumber + 1, pagesCount: pageCount, teamsCount: teams.length})}
-                <div className="drpdown">
-                    Result per page
+            {allTeams}
+            <div className="flex team-list-container">
+                <div className="flex teams-list-item align-center teams-list-footer teams-list-footer-font">
+                    <ReactPaginate
+                        previousLabel={"<"}
+                        nextLabel={">"}
+                        pageCount={pageCount}
+                        onPageChange={changePage}
+                        forcePage={pageNumber}
+                        pageRangeDisplayed={3}
+                        marginPagesDisplayed={1}
+                        containerClassName={"paginationBttns"}
+                        previousLinkClassName={"previousBttn"}
+                        nextLinkClassName={"nextBttn"}
+                        activeClassName={"paginationActive"}
+                    ></ReactPaginate>
+                    {changePageResultStatus({currentPage: pageNumber + 1, pagesCount: pageCount, teamsCount: teams.length})}
+                    <div className="result-per-page-line">
+                        Result per page
+                    </div>
+                    <div className="dropdown-per-page">
+                        <Dropdown
+                            className={"teamsperpage"}
+                            options={options}
+                            onChange={(e) => changeTeamsPerPage(e)}
+                            placeholder={options[0]}
+                        />
+                    </div>
                 </div>
-                <Dropdown
-                    className={"teamsperpage"}
-                    options={options}
-                    onChange={(e) => changeTeamsPerPage(e)}
-                    placeholder={options[0]}
-                />
-            </div>
-        </div></>
+            </div></>
+        );
+    }
+    return(
+        displayTeams()
     );
+    
 };
--- src/Pages/Admin/Teams.jsx
+++ src/Pages/Admin/Teams.jsx
@@ -1,10 +1,19 @@
 import TeamsSlector from './../../Components/Admin/Teams/TeamsSelector';
 import { TeamsList } from '../../Components/Admin/Teams/TeamsList';
+import { TopMenu } from "./../../Components/Admin/Menu/TopMenu";
+import { LeftMenu } from "../../Components/Admin/Menu/LeftMenu";
 
 export const Teams = () => {
   return (
     <div>
-      <TeamsList />
+      <TopMenu />
+      <div className="flex central-content">
+        <LeftMenu />
+        <div className="teams-central-content">
+          <TeamsSlector />
+          <TeamsList />
+        </div>
+      </div>
     </div>
   );
 };
